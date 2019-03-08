2e8e38b6726e58acee6ce8b8c0b1e27ca672b4cd
Asif Khan <asifkhan.github@gmail.com>
2019-03-08 23:45:57 +0600

    method created for showing create post view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Validator;
 
 class PostController extends Controller
 {
-    //
+    // show create post view
+    public function create(){
+        return view('create_post');
+    }
 }
