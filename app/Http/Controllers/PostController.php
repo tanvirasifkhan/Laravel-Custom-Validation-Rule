074e7ab739e447b51016fa0229a085d966875a26
Asif Khan <asifkhan.github@gmail.com>
2019-03-08 23:55:24 +0600

    store method created and rule classes imported

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use Validator;
+use App\Rules\CheckTitle;
+use App\Rules\LowerCase;
 
 class PostController extends Controller
 {
     // show create post view
     public function create(){
         return view('create_post');
+    }
+
+    // store post into the database
+    public function store(Request $request){
+        $validators=Validator::make($request->all(),[
+            'title'=>['required',new CheckTitle()],
+            'category'=>['required',new LowerCase()],
+            'description'=>'required'
+        ]);
+        if($validators->fails()){
+            return redirect()->route('post.create')->withErrors($validators)->withInput();
+        }else{
+            $post=new Post();
+            $post->title=$request->title;
+            $post->category=$request->category;
+            $post->description=$request->description;
+            $post->save();
+            return redirect()->route('post.create')->with('message','Post created successfully !');
+        }
     }
 }
